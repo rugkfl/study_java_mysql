eeaa97d0721f706a48de3cf0fcd57d21a15444cc
rugkfl <kimgyoungha61@gmail.com>
2024-04-08 08:55:04 +0000

    update

--- app/src/main/java/quests/ArithmeticsMain.java
+++ app/src/main/java/quests/ArithmeticsMain.java
@@ -5,8 +5,8 @@ import java.util.HashMap;
 public class ArithmeticsMain {
     public static void main(String[] args) {
         // 입력 받기
-        ArithmeticsInputOutput inputOutput = new ArithmeticsInputOutput();
-        HashMap<String, Integer> inputValues = inputOutput.arithmeticsInput();
+        ArithmeticsInputOutput inputOutput = new ArithmeticsInputOutput(); // ArithmeticsInputOutput의 클래스의 객체를 생성 
+        HashMap<String, Integer> inputValues = inputOutput.arithmeticsInput(); // ArithmeticsInputOutput클래스의 arithmeticsInput 메소드를 호출하여 사용자로부터 입력(HashMap 객체를 반환) 
 
         // 사칙연산
         ArithmeticsSub subs = new ArithmeticsSub();
--- app/src/main/java/quests/ArithmeticsSub.java
+++ app/src/main/java/quests/ArithmeticsSub.java
@@ -4,11 +4,11 @@ import java.util.HashMap;
 
 public class ArithmeticsSub {
     // 덧셈
-    public int arithmeticsSum(HashMap<String, Integer> inputValues){
+    public int arithmeticsSum(HashMap<String, Integer> inputValues){ // 입력받은 HashMap inputValues를 통해 계산
         int resultNumber = 0;
         
         try {
-            int firstNumber = inputValues.get("firstNumber");
+            int firstNumber = inputValues.get("firstNumber"); // HashMap의 키 값을 가져옴
             int secondNumber = inputValues.get("secondNumber");
             resultNumber = firstNumber + secondNumber ;
         } catch (Exception e) {
