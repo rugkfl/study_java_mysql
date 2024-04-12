3206d0d8f6aca8d5174e18b2d6717181bbb91892
rugkfl <kimgyoungha61@gmail.com>
2024-04-12 00:52:03 +0000

    update

--- app/src/main/java/codingtest/baekjoon/class_2588.java
+++ app/src/main/java/codingtest/baekjoon/class_2588.java
@@ -6,17 +6,26 @@ public class class_2588 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); 
         int firstNum = scanner.nextInt();
-        String secondNum = scanner.nextLine();
+        int secondNum = scanner.nextInt(); 
         
-        int[] arrNum = new int[secondNum.length()];
+        String secondNumStr = String.valueOf(secondNum); // 정수를 문자열로 변환
         
-        ArrayList<Integer> mulResult = new ArrayList<>();
-        for(int i = 0; i < arrNum.length; i++) {
-            int mulNum = firstNum * arrNum[i];
-            mulResult.add(mulNum);
+        int[] arrNum = new int[secondNumStr.length()];
+
+        for(int i =0 ; i < arrNum.length; i++ ) {
+            arrNum[i] = secondNumStr.charAt(i) - '0'; // 문자열인 숫자를 int형으로 변환하면 아스키 코드 값으로 변환 | 0은 아스키코드 48이므로 - 48을 해줘서 원하는 숫자가 되게 해야함
         }
 
-        System.out.println(mulResult);
+        
+        // ArrayList<Integer> mulResult = new ArrayList<>();
+        for(int i = arrNum.length; i > 0 ; i--) {
+            int mulNum = firstNum * arrNum[i];
+            System.out.println(mulNum);
+        }
+
+        
+
+        
 
 
         scanner.close();
